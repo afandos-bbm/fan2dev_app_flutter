d719e58a5f9fe0f9e82c71c617b1c1ade54aa954
fan2dev <alejandrofan2@gmail.com>
2024-01-30 19:52:49 +0100

    fix: :ambulance: remove widget that make app breaks

--- lib/features/about/view/about_home_page.dart
+++ lib/features/about/view/about_home_page.dart
@@ -17,79 +17,77 @@ class AboutHomePageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(height: 20),
-            Row(
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              const SizedBox(width: 20),
+              const Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage(kFan2devAboutProfilePath),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${context.l10n.about_title} 👋',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      context.l10n.about_sort_description,
+                      overflow: TextOverflow.visible,
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 20),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
               children: [
-                const SizedBox(width: 20),
-                const Padding(
-                  padding: EdgeInsets.only(top: 5),
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage(kFan2devAboutProfilePath),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${context.l10n.about_title} 👋',
-                        style: Theme.of(context).textTheme.titleLarge,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        context.l10n.about_text_1,
+                        style: Theme.of(context).textTheme.bodySmall,
                       ),
-                      const SizedBox(height: 5),
-                      Text(
-                        context.l10n.about_sort_description,
-                        overflow: TextOverflow.visible,
-                        style: Theme.of(context).textTheme.labelSmall,
-                      ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(width: 10),
+                    Image.asset(
+                      kFan2devAboutFlutterDashPath,
+                      width: 150,
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 20),
+                Text(
+                  context.l10n.about_text_2,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                const AboutImageCarrouselWidget(),
+                Text(
+                  context.l10n.about_text_3,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
               ],
             ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Flexible(
-                        child: Text(
-                          context.l10n.about_text_1,
-                          style: Theme.of(context).textTheme.bodySmall,
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Image.asset(
-                        kFan2devAboutFlutterDashPath,
-                        width: 150,
-                      ),
-                    ],
-                  ),
-                  Text(
-                    context.l10n.about_text_2,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                  const AboutImageCarrouselWidget(),
-                  Text(
-                    context.l10n.about_text_3,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 70),
-          ],
-        ),
+          ),
+          const SizedBox(height: 70),
+        ],
       ),
     );
   }
