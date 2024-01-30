e10a70b7bf4656c49344625197bbb84bc53af8a4
fan2dev <alejandrofan2@gmail.com>
2024-01-31 00:50:41 +0100

    perf: :fire: remove photos from assets

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -4,15 +4,3 @@ const kContactEmojiPath = 'assets/images/contact_emoji.png';
 const kSpanishFlagPath = 'assets/icons/spanish.svg';
 const kEnglishFlagPath = 'assets/icons/english.svg';
 const kValencianFlagPath = 'assets/icons/valencian.svg';
-
-// Photos
-const kFan2devAboutProfilePath =
-    'assets/images/photos/fan2dev_about_profile.webp';
-const kFan2devAboutFlutterDashPath =
-    'assets/images/photos/fan2dev_about_flutter_dash.webp';
-const kFan2devAboutAkiraPath = 'assets/images/photos/fan2dev_about_akira.webp';
-const kFan2devAboutArenalPath =
-    'assets/images/photos/fan2dev_about_arenal.webp';
-const kFan2devAboutCarPath = 'assets/images/photos/fan2dev_about_car.webp';
-const kFan2devAboutSwimmingPath =
-    'assets/images/photos/fan2dev_about_swimming.webp';
