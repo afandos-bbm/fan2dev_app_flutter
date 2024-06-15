27cc8c001cc8e4f03a55d3a130721ca0817bd42e
fan2dev <alejandrofan2@gmail.com>
2024-06-15 02:04:14 +0200

    fix: :rotating_light: fix linter issues

--- lib/features/auth/view/login_page.dart
+++ lib/features/auth/view/login_page.dart
@@ -3,7 +3,6 @@ import 'package:fan2dev/utils/const.dart';
 import 'package:fan2dev/utils/logger.dart';
 import 'package:fan2dev/utils/widgets/footer_f2d_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
