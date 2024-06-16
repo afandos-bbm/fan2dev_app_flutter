75e3afd308b4e9cfee04724fce4e832decc9fe2d
fan2dev <alejandrofan2@gmail.com>
2024-06-16 15:11:19 +0200

    fix: :fire: remove email check to backoffice

--- lib/features/backoffice/view/backoffice_home_page.dart
+++ lib/features/backoffice/view/backoffice_home_page.dart
@@ -53,106 +53,99 @@ class _BackofficeHomePageView extends StatelessWidget {
           ),
         ],
       ),
-      body: FirebaseAuth.instance.currentUser!.email ==
-              'alejandrofan2@gmail.com'
-          ? SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Hola, ${FirebaseAuth.instance.currentUser!.email!.split('@').first} 🚀',
-                      style: Theme.of(context).textTheme.headlineLarge,
-                      textAlign: TextAlign.start,
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Lista de formularios recibidos',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    const SizedBox(height: 20),
-                    BlocBuilder<BackofficeCubit, BackofficeCubitState>(
-                      builder: (context, state) {
-                        if (state.state == BackofficeCubitStates.loading) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hola, ${FirebaseAuth.instance.currentUser!.email!.split('@').first} 🚀',
+                style: Theme.of(context).textTheme.headlineLarge,
+                textAlign: TextAlign.start,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Lista de formularios recibidos',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 20),
+              BlocBuilder<BackofficeCubit, BackofficeCubitState>(
+                builder: (context, state) {
+                  if (state.state == BackofficeCubitStates.loading) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
 
-                        if (state.state == BackofficeCubitStates.error) {
-                          return Center(
-                            child: Text(state.error!.errorMessage),
-                          );
-                        }
+                  if (state.state == BackofficeCubitStates.error) {
+                    return Center(
+                      child: Text(state.error!.errorMessage),
+                    );
+                  }
 
-                        if (state.contactForms.isEmpty) {
-                          return const Center(
-                            child: Text('No hay formularios recibidos'),
-                          );
-                        }
-                        return ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: state.contactForms.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              margin: const EdgeInsets.all(10),
-                              elevation: 4,
-                              child: ListTile(
-                                title: Text(state.contactForms[index].subject),
-                                subtitle: Text(state.contactForms[index].email),
-                                onTap: () {
-                                  showDialog<void>(
-                                    context: context,
-                                    builder: (context) {
-                                      return AlertDialog(
-                                        title: Text(
-                                          state.contactForms[index].subject,
-                                        ),
-                                        content: Text(
-                                          state.contactForms[index].message,
-                                        ),
-                                        actions: [
-                                          TextButton(
-                                            onPressed: () {
-                                              Navigator.of(context).pop();
-                                            },
-                                            child: const Text('Cerrar'),
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );
-                                },
-                                trailing: IconButton(
-                                  icon: const Icon(Icons.delete),
-                                  onPressed: () {
-                                    context
-                                        .read<BackofficeCubit>()
-                                        .deleteContactForm(
-                                          state.contactForms[index].id!,
-                                        );
-                                  },
-                                ),
-                              ),
+                  if (state.contactForms.isEmpty) {
+                    return const Center(
+                      child: Text('No hay formularios recibidos'),
+                    );
+                  }
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: state.contactForms.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: const EdgeInsets.all(10),
+                        elevation: 4,
+                        child: ListTile(
+                          title: Text(state.contactForms[index].subject),
+                          subtitle: Text(state.contactForms[index].email),
+                          onTap: () {
+                            showDialog<void>(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text(
+                                    state.contactForms[index].subject,
+                                  ),
+                                  content: Text(
+                                    state.contactForms[index].message,
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('Cerrar'),
+                                    ),
+                                  ],
+                                );
+                              },
                             );
                           },
-                        );
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Administración de posts',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ],
-                ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {
+                              context.read<BackofficeCubit>().deleteContactForm(
+                                    state.contactForms[index].id!,
+                                  );
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
-            )
-          : const Center(
-              child: Text('No tienes permisos para acceder a esta página'),
-            ),
+              const SizedBox(height: 20),
+              Text(
+                'Administración de posts',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
